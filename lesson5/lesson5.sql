64becfd910e6f93eec8943cfdec3e6e4ed971d67
myselfadmirer <61723130+myselfadmirer@users.noreply.github.com>
2020-07-01 11:30:01 +0300

    Изменение кодировки

--- lesson5/lesson5.sql
+++ lesson5/lesson5.sql
@@ -1,217 +1,290 @@
-/* Создание таблиц */
+/* СОЗДАНИЕ ТАБЛИЦ */
+create database SHOP;
 
-create database shop;
-use shop;
+use SHOP;
 
-DROP TABLE IF EXISTS catalogs;
-CREATE TABLE catalogs (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Название раздела',
-  UNIQUE unique_name(name(10))
-) COMMENT = 'Разделы интернет-магазина';
+drop table if exists CATALOGS;
 
-INSERT INTO catalogs VALUES
-  (NULL, 'Процессоры'),
-  (NULL, 'Материнские платы'),
-  (NULL, 'Видеокарты'),
-  (NULL, 'Жесткие диски'),
-  (NULL, 'Оперативная память');
+create table CATALOGS ( ID SERIAL primary key, NAME VARCHAR(255) COMMENT 'НАЗВАНИЕ РАЗДЕЛА', unique UNIQUE_NAME(NAME(10)) ) COMMENT = 'РАЗДЕЛЫ ИНТЕРНЕТ-МАГАЗИНА';
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Имя покупателя',
-  birthday_at DATE COMMENT 'Дата рождения',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'Покупатели';
+insert
+    into
+    CATALOGS
+values (null, 'ПРОЦЕССОРЫ'),
+(null, 'МАТЕРИНСКИЕ ПЛАТЫ'),
+(null, 'ВИДЕОКАРТЫ'),
+(null, 'ЖЕСТКИЕ ДИСКИ'),
+(null, 'ОПЕРАТИВНАЯ ПАМЯТЬ');
 
-INSERT INTO users (name, birthday_at) VALUES
-  ('Геннадий', '1990-10-05'),
-  ('Наталья', '1984-11-12'),
-  ('Александр', '1985-05-20'),
-  ('Сергей', '1988-02-14'),
-  ('Иван', '1998-01-12'),
-  ('Мария', '1992-08-29');
+drop table if exists USERS;
 
-DROP TABLE IF EXISTS products;
-CREATE TABLE products (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Название',
-  description TEXT COMMENT 'Описание',
-  price DECIMAL (11,2) COMMENT 'Цена',
-  catalog_id INT UNSIGNED,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  KEY index_of_catalog_id (catalog_id)
-) COMMENT = 'Товарные позиции';
+create table USERS ( ID SERIAL primary key, NAME VARCHAR(255) COMMENT 'ИМЯ ПОКУПАТЕЛЯ', BIRTHDAY_AT DATE COMMENT 'ДАТА РОЖДЕНИЯ', CREATED_AT DATETIME default CURRENT_TIMESTAMP, UPDATED_AT DATETIME default CURRENT_TIMESTAMP on
+update
+    CURRENT_TIMESTAMP ) COMMENT = 'ПОКУПАТЕЛИ';
 
-INSERT INTO products
-  (name, description, price, catalog_id)
-VALUES
-  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
-  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
-  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
-  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
-  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
-  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
-  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+insert
+    into
+    USERS (NAME, BIRTHDAY_AT)
+values ('ГЕННАДИЙ', '1990-10-05'),
+('НАТАЛЬЯ', '1984-11-12'),
+('АЛЕКСАНДР', '1985-05-20'),
+('СЕРГЕЙ', '1988-02-14'),
+('ИВАН', '1998-01-12'),
+('МАРИЯ', '1992-08-29');
 
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders (
-  id SERIAL PRIMARY KEY,
-  user_id INT UNSIGNED,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  KEY index_of_user_id(user_id)
-) COMMENT = 'Заказы';
+drop table if exists PRODUCTS;
 
-DROP TABLE IF EXISTS orders_products;
-CREATE TABLE orders_products (
-  id SERIAL PRIMARY KEY,
-  order_id INT UNSIGNED,
-  product_id INT UNSIGNED,
-  total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'Состав заказа';
+create table PRODUCTS ( ID SERIAL primary key, NAME VARCHAR(255) COMMENT 'НАЗВАНИЕ', DESCRIPTION TEXT COMMENT 'ОПИСАНИЕ', PRICE DECIMAL (11, 2) COMMENT 'ЦЕНА', CATALOG_ID INT unsigned, CREATED_AT DATETIME default CURRENT_TIMESTAMP, UPDATED_AT DATETIME default CURRENT_TIMESTAMP on
+update
+    CURRENT_TIMESTAMP, key INDEX_OF_CATALOG_ID (CATALOG_ID) ) COMMENT = 'ТОВАРНЫЕ ПОЗИЦИИ';
 
-DROP TABLE IF EXISTS discounts;
-CREATE TABLE discounts (
-  id SERIAL PRIMARY KEY,
-  user_id INT UNSIGNED,
-  product_id INT UNSIGNED,
-  discount FLOAT UNSIGNED COMMENT 'Величина скидки от 0.0 до 1.0',
-  started_at DATETIME,
-  finished_at DATETIME,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  KEY index_of_user_id(user_id),
-  KEY index_of_product_id(product_id)
-) COMMENT = 'Скидки';
+insert
+    into
+    PRODUCTS (NAME, DESCRIPTION, PRICE, CATALOG_ID)
+values ('INTEL CORE I3-8100', 'ПРОЦЕССОР ДЛЯ НАСТОЛЬНЫХ ПЕРСОНАЛЬНЫХ КОМПЬЮТЕРОВ, ОСНОВАННЫХ НА ПЛАТФОРМЕ INTEL.', 7890.00, 1),
+('INTEL CORE I5-7400', 'ПРОЦЕССОР ДЛЯ НАСТОЛЬНЫХ ПЕРСОНАЛЬНЫХ КОМПЬЮТЕРОВ, ОСНОВАННЫХ НА ПЛАТФОРМЕ INTEL.', 12700.00, 1),
+('AMD FX-8320E', 'ПРОЦЕССОР ДЛЯ НАСТОЛЬНЫХ ПЕРСОНАЛЬНЫХ КОМПЬЮТЕРОВ, ОСНОВАННЫХ НА ПЛАТФОРМЕ AMD.', 4780.00, 1),
+('AMD FX-8320', 'ПРОЦЕССОР ДЛЯ НАСТОЛЬНЫХ ПЕРСОНАЛЬНЫХ КОМПЬЮТЕРОВ, ОСНОВАННЫХ НА ПЛАТФОРМЕ AMD.', 7120.00, 1),
+('ASUS ROG MAXIMUS X HERO', 'МАТЕРИНСКАЯ ПЛАТА ASUS ROG MAXIMUS X HERO, Z370, SOCKET 1151-V2, DDR4, ATX', 19310.00, 2),
+('GIGABYTE H310M S2H', 'МАТЕРИНСКАЯ ПЛАТА GIGABYTE H310M S2H, H310, SOCKET 1151-V2, DDR4, MATX', 4790.00, 2),
+('MSI B250M GAMING PRO', 'МАТЕРИНСКАЯ ПЛАТА MSI B250M GAMING PRO, B250, SOCKET 1151, DDR4, MATX', 5060.00, 2);
 
-DROP TABLE IF EXISTS storehouses;
-CREATE TABLE storehouses (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'Название',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'Склады';
+drop table if exists ORDERS;
 
-DROP TABLE IF EXISTS storehouses_products;
-CREATE TABLE storehouses_products (
-  id SERIAL PRIMARY KEY,
-  storehouse_id INT UNSIGNED,
-  product_id INT UNSIGNED,
-  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'Запасы на складе';
+create table ORDERS ( ID SERIAL primary key, USER_ID INT unsigned, CREATED_AT DATETIME default CURRENT_TIMESTAMP, UPDATED_AT DATETIME default CURRENT_TIMESTAMP on
+update
+    CURRENT_TIMESTAMP, key INDEX_OF_USER_ID(USER_ID) ) COMMENT = 'ЗАКАЗЫ';
 
+drop table if exists ORDERS_PRODUCTS;
 
+create table ORDERS_PRODUCTS ( ID SERIAL primary key, ORDER_ID INT unsigned, PRODUCT_ID INT unsigned, TOTAL INT unsigned default 1 COMMENT 'КОЛИЧЕСТВО ЗАКАЗАННЫХ ТОВАРНЫХ ПОЗИЦИЙ', CREATED_AT DATETIME default CURRENT_TIMESTAMP, UPDATED_AT DATETIME default CURRENT_TIMESTAMP on
+update
+    CURRENT_TIMESTAMP ) COMMENT = 'СОСТАВ ЗАКАЗА';
+
+drop table if exists DISCOUNTS;
+
+create table DISCOUNTS ( ID SERIAL primary key, USER_ID INT unsigned, PRODUCT_ID INT unsigned, DISCOUNT FLOAT unsigned COMMENT 'ВЕЛИЧИНА СКИДКИ ОТ 0.0 ДО 1.0', STARTED_AT DATETIME, FINISHED_AT DATETIME, CREATED_AT DATETIME default CURRENT_TIMESTAMP, UPDATED_AT DATETIME default CURRENT_TIMESTAMP on
+update
+    CURRENT_TIMESTAMP, key INDEX_OF_USER_ID(USER_ID), key INDEX_OF_PRODUCT_ID(PRODUCT_ID) ) COMMENT = 'СКИДКИ';
+
+drop table if exists STOREHOUSES;
+
+create table STOREHOUSES ( ID SERIAL primary key, NAME VARCHAR(255) COMMENT 'НАЗВАНИЕ', CREATED_AT DATETIME default CURRENT_TIMESTAMP, UPDATED_AT DATETIME default CURRENT_TIMESTAMP on
+update
+    CURRENT_TIMESTAMP ) COMMENT = 'СКЛАДЫ';
+
+drop table if exists STOREHOUSES_PRODUCTS;
+
+create table STOREHOUSES_PRODUCTS ( ID SERIAL primary key, STOREHOUSE_ID INT unsigned, PRODUCT_ID INT unsigned, VALUE INT unsigned COMMENT 'ЗАПАС ТОВАРНОЙ ПОЗИЦИИ НА СКЛАДЕ', CREATED_AT DATETIME default CURRENT_TIMESTAMP, UPDATED_AT DATETIME default CURRENT_TIMESTAMP on
+update
+    CURRENT_TIMESTAMP ) COMMENT = 'ЗАПАСЫ НА СКЛАДЕ';
 
 /* ДЗ */
+/* «ОПЕРАТОРЫ, ФИЛЬТРАЦИЯ, СОРТИРОВКА И ОГРАНИЧЕНИЕ» */
+/* 1. ПУСТЬ В ТАБЛИЦЕ USERS ПОЛЯ CREATED_AT И UPDATED_AT ОКАЗАЛИСЬ НЕЗАПОЛНЕННЫМИ. ЗАПОЛНИТЕ ИХ ТЕКУЩИМИ ДАТОЙ И ВРЕМЕНЕМ. */
+/* ЕСЛИ ВСЕ ЗНАЧЕНИЯ ПУСТЫЕ */
+update
+    USERS
+set
+    CREATED_AT = NOW(),
+    UPDATED_AT = NOW();
 
-/* «Операторы, фильтрация, сортировка и ограничение» */
-/* 1. Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем. */
+/* ЕСЛИ ТОЛЬКО НЕКОТОРЫЕ ЗНАЧЕНИЯ НЕ ОПРЕДЕЛЕНЫ */
+update
+    USERS
+set
+    CREATED_AT = NOW()
+where
+    CREATED_AT is null;
 
-/* если все значения пустые */
-update users set created_at = now(), updated_at = now();
+update
+    USERS
+set
+    UPDATED_AT = NOW()
+where
+    UPDATED_AT is null;
 
-/* если только некоторые значения не определены */
-update users set created_at = now() where created_at is null;
-update users set updated_at = now() where updated_at is null;
+/* 2. ТАБЛИЦА USERS БЫЛА НЕУДАЧНО СПРОЕКТИРОВАНА. ЗАПИСИ CREATED_AT И UPDATED_AT БЫЛИ ЗАДАНЫ ТИПОМ VARCHAR, 
+ * И В НИХ ДОЛГОЕ ВРЕМЯ ПОМЕЩАЛИСЬ ЗНАЧЕНИЯ В ФОРМАТЕ 20.10.2017 8:10. 
+ * НЕОБХОДИМО ПРЕОБРАЗОВАТЬ ПОЛЯ К ТИПУ DATETIME, СОХРАНИВ ВВЕДЁННЫЕ РАНЕЕ ЗНАЧЕНИЯ. */
+/* ТАК КАК ТИП ДАННЫХ ИЗНАЧАЛЬНО УЖЕ БЫЛ ЗАДАН КАК DATETIME, ИЗМЕНИМ ЕГО СНАЧАЛА НА VARCHAR НА ПРИМЕРЕ CREATED_AT */
+desc USERS;
 
-/* 2. Таблица users была неудачно спроектирована. Записи created_at и updated_at были заданы типом VARCHAR, 
- * и в них долгое время помещались значения в формате 20.10.2017 8:10. 
- * Необходимо преобразовать поля к типу DATETIME, сохранив введённые ранее значения. */
+alter table USERS modify column CREATED_AT VARCHAR(255);
 
-/* Так как тип данных изначально уже был задан как datetime, изменим его сначала на varchar на примере created_at */
+select
+    *
+from
+    USERS;
 
-desc users;
-alter table users modify column created_at varchar(255);
-select * from users;
+/* ПРИВОДИМ ДАННЫЕ К ФОРМАТУ 20.10.2017 8:10 */
+update
+    USERS
+set
+    CREATED_AT = DATE_FORMAT( CREATED_AT, '%D.%M.%Y %H:%I' );
 
-/* Приводим данные к формату 20.10.2017 8:10 */
+/* ТАК КАК ЭТО СЕЙЧАС СТРОКА, ТО ПРИВЕДЕМ ЕЕ К ФОРМАТУ ДАТЫ С ПОМОЩЬЮ ФУНКЦИИ STR_TO_DATE, ПОСЛЕ ЧЕГО МОДНО ВЕРНУТЬ ТИП DATETIME */
+update
+    USERS
+set
+    CREATED_AT = STR_TO_DATE( CREATED_AT, '%D.%M.%Y %H:%I' );
 
-update users set created_at = DATE_FORMAT( created_at, '%d.%m.%Y %H:%i' );
+alter table USERS modify column CREATED_AT DATETIME;
 
-/* Так как это сейчас строка, то приведем ее к формату даты с помощью функции str_to_date, после чего модно вернуть тип datetime */
-update users set created_at = str_to_date( created_at, '%d.%m.%Y %H:%i' );
-alter table users modify column created_at datetime;
+/* 3. В ТАБЛИЦЕ СКЛАДСКИХ ЗАПАСОВ STOREHOUSES_PRODUCTS В ПОЛЕ VALUE МОГУТ ВСТРЕЧАТЬСЯ САМЫЕ РАЗНЫЕ ЦИФРЫ: 0, ЕСЛИ ТОВАР ЗАКОНЧИЛСЯ И ВЫШЕ НУЛЯ,
+ * ЕСЛИ НА СКЛАДЕ ИМЕЮТСЯ ЗАПАСЫ. НЕОБХОДИМО ОТСОРТИРОВАТЬ ЗАПИСИ ТАКИМ ОБРАЗОМ, ЧТОБЫ ОНИ ВЫВОДИЛИСЬ В ПОРЯДКЕ УВЕЛИЧЕНИЯ ЗНАЧЕНИЯ VALUE.
+ * ОДНАКО НУЛЕВЫЕ ЗАПАСЫ ДОЛЖНЫ ВЫВОДИТЬСЯ В КОНЦЕ, ПОСЛЕ ВСЕХ ЗАПИСЕЙ. */
+/* ТАК КАК ТАБЛИЦА ИЗНАЧАЛЬНО ПУСТАЯ, ЗАПОЛНИМ ЕЕ СЛУЧАЙНЫМИ ЗНАЧЕНИЯМИ */
+insert
+    into
+    STOREHOUSES_PRODUCTS (STOREHOUSE_ID , PRODUCT_ID , VALUE )
+values (FLOOR(1 + RAND() * 5), (
+select
+    ID
+from
+    PRODUCTS
+order by
+    RAND()
+limit 1), FLOOR(RAND() * 50)),
+(FLOOR(1 + RAND() * 5), (
+select
+    ID
+from
+    PRODUCTS
+order by
+    RAND()
+limit 1), FLOOR(RAND() * 30)),
+(FLOOR(1 + RAND() * 5), (
+select
+    ID
+from
+    PRODUCTS
+order by
+    RAND()
+limit 1), FLOOR(RAND() * 100)),
+(FLOOR(1 + RAND() * 5), (
+select
+    ID
+from
+    PRODUCTS
+order by
+    RAND()
+limit 1), FLOOR(RAND() * 50)),
+(FLOOR(1 + RAND() * 5), (
+select
+    ID
+from
+    PRODUCTS
+order by
+    RAND()
+limit 1), FLOOR(RAND() * 80)),
+(FLOOR(1 + RAND() * 5), (
+select
+    ID
+from
+    PRODUCTS
+order by
+    RAND()
+limit 1), FLOOR(RAND() * 50)),
+(FLOOR(1 + RAND() * 5), (
+select
+    ID
+from
+    PRODUCTS
+order by
+    RAND()
+limit 1), FLOOR(RAND() * 20));
 
+select
+    *
+from
+    STOREHOUSES_PRODUCTS;
 
-/* 3. В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0, если товар закончился и выше нуля,
- * если на складе имеются запасы. Необходимо отсортировать записи таким образом, чтобы они выводились в порядке увеличения значения value.
- * Однако нулевые запасы должны выводиться в конце, после всех записей. */
+/* ПРОИЗВОДИМ СОРТИРОВКУ В 2 ЭТАПА: СНАЧАЛА НУЛЕВЫЕ ЗНАЧЕНИЯ, ЗАТЕМ - ОСТАЛЬНЫЕ */
+select
+    *
+from
+    STOREHOUSES_PRODUCTS
+order by
+    VALUE = 0,
+    VALUE;
 
-/* Так как таблица изначально пустая, заполним ее случайными значениями */
+/* 4. (ПО ЖЕЛАНИЮ) ИЗ ТАБЛИЦЫ USERS НЕОБХОДИМО ИЗВЛЕЧЬ ПОЛЬЗОВАТЕЛЕЙ, РОДИВШИХСЯ В АВГУСТЕ И МАЕ. 
+ * МЕСЯЦЫ ЗАДАНЫ В ВИДЕ СПИСКА АНГЛИЙСКИХ НАЗВАНИЙ (MAY, AUGUST) */
+-- SELECT NAME, DATE_FORMAT(BIRTHDAY_AT, '%M') FROM USERS; - ПРОВЕРКА ИЗВЛЕЧЕНИЯ МЕСЯЦА
+ /* ВЫБОРКА ПО ДАТАМ РОЖДЕНИЯ (МАЙ, АВГУСТ) */
+select
+    *
+from
+    USERS
+where
+    DATE_FORMAT(BIRTHDAY_AT, '%M') in ('MAY', 'AUGUST');
 
-insert into storehouses_products (storehouse_id , product_id , value ) values 
-(floor(1 + rand() * 5), (select id from products order by rand() limit 1), floor(rand() * 50)),
-(floor(1 + rand() * 5), (select id from products order by rand() limit 1), floor(rand() * 30)),
-(floor(1 + rand() * 5), (select id from products order by rand() limit 1), floor(rand() * 100)),
-(floor(1 + rand() * 5), (select id from products order by rand() limit 1), floor(rand() * 50)),
-(floor(1 + rand() * 5), (select id from products order by rand() limit 1), floor(rand() * 80)),
-(floor(1 + rand() * 5), (select id from products order by rand() limit 1), floor(rand() * 50)),
-(floor(1 + rand() * 5), (select id from products order by rand() limit 1), floor(rand() * 20));
+/* 5. (ПО ЖЕЛАНИЮ) ИЗ ТАБЛИЦЫ CATALOGS ИЗВЛЕКАЮТСЯ ЗАПИСИ ПРИ ПОМОЩИ ЗАПРОСА. 
+ * SELECT * FROM CATALOGS WHERE ID IN (5, 1, 2); ОТСОРТИРУЙТЕ ЗАПИСИ В ПОРЯДКЕ, ЗАДАННОМ В СПИСКЕ IN. */
+select
+    *
+from
+    CATALOGS
+where
+    ID in (5, 1, 2)
+order by
+    FIELD(ID, 5, 1, 2);
 
-select * from storehouses_products;
+/* «АГРЕГАЦИЯ ДАННЫХ» */
+/* 1. ПОДСЧИТАЙТЕ СРЕДНИЙ ВОЗРАСТ ПОЛЬЗОВАТЕЛЕЙ В ТАБЛИЦЕ USERS. */
+/* ИЗВЛЕЧЕНИЕ ГОДА РОЖДЕНИЯ ПОЛЬЗОВАТЕЛЕЙ ДЛЯ ПРОВЕРКИ */
+select
+    NAME,
+    DATE_FORMAT(BIRTHDAY_AT, '%Y')
+from
+    USERS;
 
-/* Производим сортировку в 2 этапа: сначала нулевые значения, затем - остальные */
+/* ПРОВЕРКА ИЗВЛЕЧЕНИЯ ТЕКУЩЕГО ГОДА */
+select
+    DATE_FORMAT(NOW(), '%Y');
 
-select * from storehouses_products order by value = 0, value;
+/* ВЫЧИСЛЕНИЕ ВОЗРАСТА ПОЛЬЗОВАТЕЛЕЙ */
+select
+    NAME,
+    (year(NOW()) - DATE_FORMAT(BIRTHDAY_AT, '%Y')) as BIRTHDAY_AT
+from
+    USERS;
 
+/* ПОДСЧЕТ СРЕДНЕГО ВОЗРАСТА */
+select
+    AVG((year(NOW()) - DATE_FORMAT(BIRTHDAY_AT, '%Y'))) as AV_AGE
+from
+    USERS;
 
-/* 4. (по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае. 
- * Месяцы заданы в виде списка английских названий (may, august) */
+/* 2. ПОДСЧИТАЙТЕ КОЛИЧЕСТВО ДНЕЙ РОЖДЕНИЯ, КОТОРЫЕ ПРИХОДЯТСЯ НА КАЖДЫЙ ИЗ ДНЕЙ НЕДЕЛИ. СЛЕДУЕТ УЧЕСТЬ, 
+ * ЧТО НЕОБХОДИМЫ ДНИ НЕДЕЛИ ТЕКУЩЕГО ГОДА, А НЕ ГОДА РОЖДЕНИЯ. */
+select
+    *
+from
+    USERS;
 
--- SELECT name, date_format(birthday_at, '%M') FROM users; - проверка извлечения месяца
+/* ПОДСЧЕТ ПОЛЬЗОВАТЕЛЕЙ С ВВОДОМ ГОДА ВРУЧНУЮ (МОЖНО ПОДСЧИТАТЬ ДЛЯ ЛЮБОГО ГОДА */
+select
+    COUNT(*),
+    DATE_FORMAT(DATE_FORMAT(BIRTHDAY_AT, '2020-%M-%D') , '%W') as WEEK_DAY
+from
+    USERS
+group by
+    WEEK_DAY;
 
-/* Выборка по датам рождения (май, август) */
+/* ПОДСЧЕТ ПОЛЬЗОВАТЕЛЕЙ С АВТОМАТИЧЕСКИМ ВВОДОМ ТЕКУЩЕГО ГОДА */
+select
+    COUNT(*),
+    DATE_FORMAT(DATE_FORMAT(BIRTHDAY_AT, CONCAT(year(NOW()), '-%M-%D')) , '%W') as WEEK_DAY
+from
+    USERS
+group by
+    WEEK_DAY;
 
-SELECT * FROM users WHERE date_format(birthday_at, '%M') IN ('may', 'august');
-
-/* 5. (по желанию) Из таблицы catalogs извлекаются записи при помощи запроса. 
- * SELECT * FROM catalogs WHERE id IN (5, 1, 2); Отсортируйте записи в порядке, заданном в списке IN. */
-
-SELECT * FROM catalogs WHERE id IN (5, 1, 2) order by field(id, 5, 1, 2);
-
-/* «Агрегация данных» */
-
-/* 1. Подсчитайте средний возраст пользователей в таблице users. */
-
-/* Извлечение года рождения пользователей для проверки */
-
-select name, date_format(birthday_at, '%Y') from users;
-
-/* Проверка извлечения текущего года */
-
-select date_format(now(), '%Y'); 
-
-/* Вычисление возраста пользователей */
-
-select name, (year(now()) - date_format(birthday_at, '%Y')) as birthday_at from users;
-
-/* Подсчет среднего возраста */
-
-select avg((year(now()) - date_format(birthday_at, '%Y'))) as av_age from users;
-
-/* 2. Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. Следует учесть, 
- * что необходимы дни недели текущего года, а не года рождения. */
-
-select * from users;
-
-/* Подсчет пользователей с вводом года вручную (можно подсчитать для любого года */
-
-select count(*), date_format(date_format(birthday_at, '2020-%m-%d') , '%W') as week_day from users GROUP by week_day;
-
-/* Подсчет пользователей с автоматическим вводом текущего года */
-
-select count(*), date_format(date_format(birthday_at, concat(year(now()), '-%m-%d')) , '%W') as week_day from users GROUP by week_day;
-
-/*3. (по желанию) Подсчитайте произведение чисел в столбце таблицы.  */
-
-/* Подсчитаем произведение значений в столбце id таблицы users */
-
-SELECT floor(exp(sum(ln(id)))) as mult FROM users;
+/*3. (ПО ЖЕЛАНИЮ) ПОДСЧИТАЙТЕ ПРОИЗВЕДЕНИЕ ЧИСЕЛ В СТОЛБЦЕ ТАБЛИЦЫ.  */
+/* ПОДСЧИТАЕМ ПРОИЗВЕДЕНИЕ ЗНАЧЕНИЙ В СТОЛБЦЕ ID ТАБЛИЦЫ USERS */
+select
+    FLOOR(exp(SUM(ln(ID)))) as MULT
+from
+    USERS;
